47538f4153c5dcff80f126430cc489023078e948
jisooou <jisoou4@gmail.com>
2025-11-03 01:22:41 +0900

    feat(Statistics): 출력에 필요한 필드를 위한 Model 추가

---
+++ src/main/java/lotto/model/Statistics.java
@@ -0,0 +1,25 @@
+package lotto.model;
+
+public class Statistics {
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int prize;
+
+    public Statistics(int matchCount, boolean bonusMatch, int prize) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
