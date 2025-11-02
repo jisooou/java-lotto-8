c108b235542808d946920822a187778c64bf2bd4
jisooou <jisoou4@gmail.com>
2025-11-03 01:09:34 +0900

    feat(ErrorCode): 예외 처리 추가

--- src/main/java/lotto/exception/ErrorCode.java
+++ src/main/java/lotto/exception/ErrorCode.java
@@ -3,12 +3,16 @@ package lotto.exception;
 public enum ErrorCode {
     LOTTO_NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1~45 사이어야 합니다."),
     LOTTO_OUT_OF_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
-    LOTTO_NO_DUPLICATE("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    LOTTO_NO_DUPLICATE("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    PURCHASE_PRICE_NOT_NUMBER("[ERROR] 구매 금액에는 문자열이 들어갈 수 없습니다."),
+    PURCHASE_PRICE_OUT_OF_RANGE("[ERROR] 구매 금액은 0 이하일 수 없습니다."),
+    PURCHASE_PRICE_OUT_OF_UNIT("[ERROR] 구매 금액은 1,000원 단위여야 합니다."),
+    BONUS_NUMBER_NO_DUPLICATE("[ERROR] 보너스 번호와 로또 번호는 중복될 수 없습니다.");
 
 
     private final String message;
 
-    ErrorCode(final String message) {
+    ErrorCode(String message) {
         this.message = message;
     }
 
