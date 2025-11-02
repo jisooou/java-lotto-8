92ade001f48df7ed6560366d7b2a55d5a09108ef
jisooou <jisoou4@gmail.com>
2025-11-03 00:09:27 +0900

    feat(ErrorCode): Lotto 예외 처리 추가

--- src/main/java/lotto/exception/ErrorCode.java
+++ src/main/java/lotto/exception/ErrorCode.java
@@ -1,7 +1,10 @@
 package lotto.exception;
 
 public enum ErrorCode {
-    LOTTO_NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1~45 사이어야 합니다.");
+    LOTTO_NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1~45 사이어야 합니다."),
+    LOTTO_OUT_OF_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+    LOTTO_NO_DUPLICATE("[ERROR] 로또 번호는 중복될 수 없습니다.");
+
 
     private final String message;
 
