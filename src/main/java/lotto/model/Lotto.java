7a3a74680ccb3a5774aece102f5732be8a021387
jisooou <jisoou4@gmail.com>
2025-11-03 00:03:25 +0900

    feat(Lotto): 1개의 로또 기능 구현 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,38 @@
-package lotto;
+package lotto.model;
+
+import lotto.exception.ErrorCode;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private static final int LOTTO_SIZE = 6;
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorCode.LOTTO_OUT_OF_SIZE.getMessage());
+        }
+        if (numbers.stream().distinct().count() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorCode.LOTTO_NO_DUPLICATE.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getLottoNumbers() {
+        return numbers.stream()
+                .map(LottoNumber::getLottoNumber)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return getLottoNumbers().toString();
+    }
 }
