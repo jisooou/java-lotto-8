9902f48a7f34ce34225e074d6f55c2e8608c7a9b
jisooou <jisoou4@gmail.com>
2025-11-03 01:08:03 +0900

    feat(BonusNumber): 보너스 번호를 저장하기 위한 Model 추가

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,4 +1,27 @@
 package lotto.model;
 
+import lotto.exception.ErrorCode;
+
 public class BonusNumber {
+    private final static int MIN = 1;
+    private final static int MAX = 45;
+    private final int bonusNumber;
+
+    public BonusNumber(int bonusNumber, WinnerNumber winnerNumber) {
+        validate(bonusNumber, winnerNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(int bonusNumber, WinnerNumber winnerNumber) {
+        if (bonusNumber < MIN || bonusNumber > MAX) {
+            throw new IllegalArgumentException(ErrorCode.LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
+        }
+        if (winnerNumber.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorCode.BONUS_NUMBER_NO_DUPLICATE.getMessage());
+        }
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }
