e7f27186b46a656bf59a6139f09b9fbd785afad2
jisooou <jisoou4@gmail.com>
2025-11-03 01:07:34 +0900

    feat(WinnerNumber): 당첨 로또 번호를 저장하기 위한 Model 추가

---
+++ src/main/java/lotto/model/WinnerNumber.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+import lotto.exception.ErrorCode;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinnerNumber {
+    private static final int WINNER_SIZE = 6;
+    private final List<Integer> numbers;
+
+    public WinnerNumber(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = List.copyOf(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != WINNER_SIZE) {
+            throw new IllegalArgumentException(ErrorCode.LOTTO_OUT_OF_SIZE.getMessage());
+        }
+        if (numbers.stream().distinct().count() != WINNER_SIZE) {
+            throw new IllegalArgumentException(ErrorCode.LOTTO_NO_DUPLICATE.getMessage());
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
