f91022f2187e82ddb660e589e0d459848571bcde
jisooou <jisoou4@gmail.com>
2025-11-03 15:22:46 +0900

    feat(Application): Controller를 호출하는 main 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(
+                new InputView(),
+                new OutputView(),
+                new LottoService()
+        );
+
+        try {
+            lottoController.run();
+        } finally {
+            Console.close();
+        }
+
     }
 }
