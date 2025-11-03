1495f57564216d6391cecd4b938cf830ff055829
jisooou <jisoou4@gmail.com>
2025-11-03 17:48:38 +0900

    feat(LottoService): 로또 관련 로직 수행하기 위한 메서드 추가(로또 발급, 일치 여부, 통계, 수익률 계산)

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,4 +1,63 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.model.*;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class LottoService {
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+    private static final int LOTTO_SIZE = 6;
+
+    public List<Lotto> generateLotto(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> number = Randoms.pickUniqueNumbersInRange(MIN, MAX, LOTTO_SIZE)
+                    .stream()
+                    .sorted()
+                    .collect(Collectors.toList());
+            lottos.add(new Lotto(number));
+        }
+        return lottos;
+    }
+
+    public Statistics lottoResult(Lotto lotto, WinnerNumber winnerNumber, BonusNumber bonusNumber) {
+        List<Integer> lottoNumbers = lotto.getLottoNumbers();
+        List<Integer> winnerNumbers = winnerNumber.getNumbers();
+
+        int matchCount = 0;
+        for (int number : lottoNumbers) {
+            if (winnerNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+        boolean bonusMatch = lottoNumbers.contains(bonusNumber.getBonusNumber());
+        return Statistics.findRank(matchCount, bonusMatch);
+    }
+
+    public Map<Statistics, Integer> allLottoResult(List<Lotto> lottos, WinnerNumber winnerNumber, BonusNumber bonusNumber) {
+        Map<Statistics, Integer> result = new EnumMap<>(Statistics.class);
+        for (Statistics s : Statistics.values()) {
+            result.put(s, 0);
+        }
+        for (Lotto lotto : lottos) {
+            Statistics rank = lottoResult(lotto, winnerNumber, bonusNumber);
+            result.put(rank, result.get(rank) + 1);
+        }
+        return result;
+    }
+
+    public double calculateProfit(Map<Statistics, Integer> result, int purchaseAmount) {
+        long totalPrize = 0;
+        for (Statistics s : result.keySet()) {
+            totalPrize += (long) s.getPrize() * result.get(s);
+        }
+        double profit = (double) totalPrize / purchaseAmount * 100;
+        return Math.round(profit * 10) / 10.0;
+    }
 }
