5ec7d3236eef1686fcfc780ee026210301d52dd8
jisooou <jisoou4@gmail.com>
2025-11-03 18:04:13 +0900

    feat(LottoController): inputView, outputView, lottoService 로직 추가 및 메서드 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,60 @@
 package lotto.controller;
 
+import lotto.model.*;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+import java.util.Map;
+
 public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoService lottoService;
+
+    public LottoController(InputView inputView, OutputView outputView, LottoService lottoService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoService = lottoService;
+    }
+
+    public void run() {
+        try {
+            PurchasePrice purchasePrice = getPurchasePrice();
+            List<Lotto> lottos = generateAndPrintLotto(purchasePrice);
+
+            WinnerNumber winnerNumber = getWinnerNumber();
+            BonusNumber bonusNumber = getBonusNumber(winnerNumber);
+
+            printResult(lottos, winnerNumber, bonusNumber, purchasePrice);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            run();
+        }
+    }
+
+    private PurchasePrice getPurchasePrice() {
+        return new PurchasePrice(inputView.readPurchasePrice());
+    }
+
+    private List<Lotto> generateAndPrintLotto(PurchasePrice purchasePrice) {
+        List<Lotto> lottos = lottoService.generateLotto(purchasePrice.getCount());
+        outputView.printLotto(lottos);
+        return lottos;
+    }
+
+    private WinnerNumber getWinnerNumber() {
+        return new WinnerNumber(inputView.readWinningNumber());
+    }
+
+    private BonusNumber getBonusNumber(WinnerNumber winnerNumber) {
+        return new BonusNumber(inputView.readBonusNumber(), winnerNumber);
+    }
+
+    private void printResult(List<Lotto> lottos, WinnerNumber winnerNumber, BonusNumber bonusNumber, PurchasePrice purchasePrice) {
+        Map<Statistics, Integer> result = lottoService.allLottoResult(lottos, winnerNumber, bonusNumber);
+        double profit = lottoService.calculateProfit(result, purchasePrice.getAmount());
+        outputView.printResult(result, profit);
+    }
 }
