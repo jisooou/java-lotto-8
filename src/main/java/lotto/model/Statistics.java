1d61f237de376e8b656d06ac0fa7442878d6bc81
jisooou <jisoou4@gmail.com>
2025-11-03 16:24:24 +0900

    feat(Statistics): Enum으로 관리하는 로또 통계 메서드 추가

--- src/main/java/lotto/model/Statistics.java
+++ src/main/java/lotto/model/Statistics.java
@@ -1,25 +1,33 @@
 package lotto.model;
 
-public class Statistics {
+public enum Statistics {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
     private final int matchCount;
     private final boolean bonusMatch;
     private final int prize;
 
-    public Statistics(int matchCount, boolean bonusMatch, int prize) {
+    Statistics(int matchCount, boolean bonusMatch, int prize) {
         this.matchCount = matchCount;
         this.bonusMatch = bonusMatch;
         this.prize = prize;
     }
 
-    public int getMatchCount() {
-        return matchCount;
-    }
-
-    public boolean isBonusMatch() {
-        return bonusMatch;
-    }
-
     public int getPrize() {
         return prize;
+    }
+
+    public static Statistics findRank(int matchCount, boolean bonusMatch) {
+        if (matchCount == 6) return FIRST;
+        if (matchCount == 5 && bonusMatch) return SECOND;
+        if (matchCount == 5) return THIRD;
+        if (matchCount == 4) return FOURTH;
+        if (matchCount == 3) return FIFTH;
+        return NONE;
     }
 }
