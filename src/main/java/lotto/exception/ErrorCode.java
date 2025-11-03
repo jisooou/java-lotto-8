44f6153b0a44a72b732806fdc80f94571207247f
jisooou <jisoou4@gmail.com>
2025-11-03 13:46:56 +0900

    fix(ErrorCode): 에러 문구 수정

--- src/main/java/lotto/exception/ErrorCode.java
+++ src/main/java/lotto/exception/ErrorCode.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public enum ErrorCode {
-    LOTTO_NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1~45 사이어야 합니다."),
+    LOTTO_NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     LOTTO_OUT_OF_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
     LOTTO_NO_DUPLICATE("[ERROR] 로또 번호는 중복될 수 없습니다."),
     PURCHASE_PRICE_NOT_NUMBER("[ERROR] 구매 금액에는 문자열이 들어갈 수 없습니다."),
